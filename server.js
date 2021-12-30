d490bd2788de6132bf305647118cf527be37582f
Malte Teichert <malte@malteteichert.de>
2021-12-30 19:35:49 +0100

    + fs and https requirements

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const fs = require('fs');
+const https = require('https');
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
