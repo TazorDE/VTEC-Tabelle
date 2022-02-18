e2ed9651531defe238d159576ec6a71d378baf88
Malte Teichert <malte@malteteichert.de>
2022-02-18 02:19:06 +0100

    Reworked Structure of DB Interaction
    removes most ejs files
    creates structure for new db interaction
    implements most functions for db interaction
    set up placeholders for new db interactions

---
+++ src/ts/cloudant-interaction.ts
@@ -0,0 +1,355 @@
+import { CloudantV1 } from '@ibm-cloud/cloudant';
+require('dotenv').config();
+import Uuid from 'uuid';
+import countryList from 'country-list';
+
+const userdb: string = "vtec-users"
+const driverdb: string = "vtec-drivers"
+const eventdb: string = "vtec-events"
+const seasondb: string = "vtec-seasons"
+
+const client = CloudantV1.newInstance({});
+
+// -----------------------------------------------------------
+// Type definitions
+// -----------------------------------------------------------
+
+type raceResult = {
+    drivers: string[];
+    raceFinishOrder: string[];
+    fastestLap: string;
+    fastestLapTime: number;
+    raceDNF: string[];
+    raceDQ: string[];
+    qualiFinishOrder?: string[];
+    qualiFastestTime?: number;
+    qualiDQ?: string[];
+}
+
+type season = {
+    year: string,
+    season: string,
+    teams: [
+        {
+            name: string,
+            drivers: string[],
+            color: string
+        }
+    ],
+    events: [
+        {
+            id: string,
+            date: Date,
+            result: raceResult
+        }
+    ],
+    createdAt: Date,
+    updatedAt: Date,
+    fastestLapPoints: number,
+    fastestLapPosition: number,
+    pointsDistribution: number[],
+    pointsDistributionQuali?: number[]
+}
+
+// -----------------------------------------------------------
+// Users
+// -----------------------------------------------------------
+
+//create a new user
+async function createUser(name: any, email: any, password: any) {
+    //check if user already exists
+    const result = await findUserByEmail(email);
+    if (result.length > 0) {
+        return null;
+    }
+    else {
+        //create a new user
+        return client.postDocument({
+            db: userdb,
+            document: {
+                _id: Uuid.v4(),
+                name: name,
+                email: email,
+                password: password,
+                createdAt: new Date()
+            }
+        });
+    }
+}
+
+//get all user documents
+async function getAllUsers() {
+    return await client.postAllDocs({
+        db: userdb,
+        includeDocs: true
+    })
+}
+
+//find user by id
+async function findUserById(id: any) {
+    let data = await getAllUsers();
+    return data.result.rows.filter(user => { user.doc?._id === id });
+}
+
+//find user by email
+async function findUserByEmail(email: any) {
+    let data = await getAllUsers();
+    return data.result.rows.filter(user => user.doc?.email === email);
+}
+
+// -----------------------------------------------------------
+// Drivers
+// -----------------------------------------------------------
+
+// create a new driver
+async function createDriver(name: string) {
+    return client.postDocument({
+        db: driverdb,
+        document: {
+            name: name,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+    });
+}
+
+// get all drivers
+async function getAllDrivers() {
+    return await client.postAllDocs({
+        db: driverdb,
+        includeDocs: true
+    });
+}
+
+// find driver by id
+async function findDriverById(id: string) {
+    let data = await getAllDrivers();
+    return data.result.rows.filter((driver) => { return driver.doc?._id === id });
+}
+
+// update a driver
+async function updateDriver(id: string, name: string) {
+    return client.postDocument({
+        db: driverdb,
+        document: {
+            _id: id,
+            name: name,
+            updatedAt: new Date()
+        }
+    });
+}
+
+// -----------------------------------------------------------
+// Events
+// -----------------------------------------------------------
+
+// create a new event
+async function createEvent(threeLetterCode: string, country: string) {
+
+    // check if country exists
+    let countryExists = await checkCountryExists(country);
+    if (countryExists) {
+        // country exists, creating event
+        return client.postDocument({
+            db: eventdb,
+            document: {
+                threeLetterCode: threeLetterCode,
+                country: country,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        });
+    } else {
+        // country does not exist
+        return null;
+    }
+}
+
+// check if country exists
+async function checkCountryExists(country: string) {
+    let countries = countryList.getNames();
+    if (countries.includes(country)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// get all events
+async function getAllEvents() {
+    return await client.postAllDocs({
+        db: eventdb,
+        includeDocs: true
+    });
+}
+
+// find event by id
+async function findEventById(id: string) {
+    let data = await getAllEvents();
+    return data.result.rows.filter((event) => { return event.doc?._id === id });
+}
+
+// update an event
+async function updateEvent(id: string, threeLetterCode: string, country: string) {
+
+    // check if country exists
+    let countryExists = await checkCountryExists(country);
+    if (countryExists) {
+        // country exists, updating event
+        return client.postDocument({
+            db: eventdb,
+            document: {
+                _id: id,
+                threeLetterCode: threeLetterCode,
+                country: country,
+                updatedAt: new Date()
+            }
+        });
+    } else {
+        // country does not exist
+        return null;
+    }
+}
+
+// -----------------------------------------------------------
+// Seasons
+// -----------------------------------------------------------
+
+// create a new season
+async function createSeason(
+    year: string,
+    seasonNr: string,
+    teams: [{ name: string, drivers: [string], color: string }],
+    events: [{ id: string, date: Date, result: raceResult }],
+    quali: boolean,
+    fastestLapPoint: number,
+    fastestLapPosition: number, // highest position for the fastest lap point to be awarded
+    pointsDistribution: [], // value of points for each position in the race. First Arr Element is mapped to first position
+    pointsDistributionQuali?: [], // equivalent to pointsDistribution    
+) {
+    // generate results object
+    let result: raceResult = {
+        drivers: [
+            /* All drivers that participated in the race, and the respective team they drove for
+                {
+                    id: string, // driver id
+                    team: string, // team name
+                    teamColor: string
+                }
+            */
+        ],
+        raceFinishOrder: [], // array of drivers ids
+        fastestLap: "", // driver id
+        fastestLapTime: 0, // time in seconds
+        raceDNF: [], // array of drivers ids
+        raceDQ: [], // array of drivers ids
+    }
+    if (quali) {
+        result.qualiFinishOrder = [];
+        result.qualiDQ = [];
+        result.qualiFastestTime = 0;
+    }
+    // append results to each event
+    events.forEach(event => event.result = result);
+
+    // create season object
+    let season: season = {
+        year: year,
+        season: seasonNr,
+        teams,
+        events,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        pointsDistribution: pointsDistribution,
+        fastestLapPoints: fastestLapPoint,
+        fastestLapPosition: fastestLapPosition,
+    }
+    if (quali) {
+        season.pointsDistributionQuali = pointsDistributionQuali;
+    }
+
+    // check if season already exists
+    let seasonExists = await checkSeasonExists(year, seasonNr);
+    if (seasonExists) {
+        return null;
+    } else {
+        // season does not exist, creating season
+        return client.postDocument({
+            db: seasondb,
+            document: season
+        });
+    }
+}
+
+// get all seasons
+async function getAllSeasons() {
+    return await client.postAllDocs({
+        db: seasondb,
+        includeDocs: true
+    });
+}
+
+// get all seasons for a specific year
+async function getAllSeasonsForYear(year: string) {
+    let data = await getAllSeasons();
+    return data.result.rows.filter(season => season.doc?.year === year);
+}
+
+// get season by year and season number
+async function getSeasonByYearAndSeason(year: string, seasonNr: string) {
+    let data = await getAllSeasons();
+    return data.result.rows.filter(season => season.doc?.year === year && season.doc?.season === seasonNr);
+}
+
+// check if season exists
+async function checkSeasonExists(year: string, seasonNr: string) {
+    let data = await getAllSeasons();
+    return data.result.rows.filter(season => season.doc?.year === year && season.doc?.season === seasonNr).length > 0;
+}
+
+// add new result to season
+async function addResultToSeason(year: string, seasonNr: string, event: string, result: raceResult) {
+
+}
+
+// update result in season
+async function updateResultInSeason(year: string, seasonNr: string, event: string, result: raceResult) {
+
+}
+
+// update a season
+async function updateSeason(
+    year: string,
+    seasonNr: string,
+    teams: [{ name: string, drivers: [string], color: string }],
+    events: [{ id: string, date: Date, result: raceResult }],
+    quali: boolean,
+    fastestLapPoint: number,
+    fastestLapPosition: number, // highest position for the fastest lap point to be awarded
+    pointsDistribution: [], // value of points for each position in the race. First Arr Element is mapped to first position
+    pointsDistributionQuali?: [], // equivalent to pointsDistribution    
+) {
+
+}
+
+module.exports = {
+    createUser,
+    findUserById,
+    findUserByEmail,
+    createDriver,
+    findDriverById,
+    getAllDrivers,
+    updateDriver,
+    createEvent,
+    findEventById,
+    getAllEvents,
+    updateEvent,
+    createSeason,
+    getAllSeasons,
+    getAllSeasonsForYear,
+    getSeasonByYearAndSeason,
+    checkSeasonExists,
+    addResultToSeason,
+    updateResultInSeason,
+    updateSeason
+}
